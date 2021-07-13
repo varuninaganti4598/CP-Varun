d2b4bdcb4015bc924e855268c207e3ca871a9fc7
varuninaganti4598 <varuninaganti529@msitprogram.net>
2021-07-13 20:00:18 +0530

    Commit 1

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -4,8 +4,38 @@
 # the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
 # so the 0th digit is the rightmost digit. 
 
-
+def check(n,k,d):
+	emp=[]
+	if(n>0):
+		while(n>0):
+			a=n%10
+			emp.append(a)
+			n=n//10
+		print(emp)
+	if(k>=len(emp)):
+		emp.append(d)
+		result=emp[::-1]
+		print(result)
+		e=""
+		for i in range(len(emp)):
+			e+=str(result[i])
+		return int(e)
+	else:
+		emp[k]=d
+		result=emp[::-1]
+		e=""
+		for i in range(len(emp)):
+			e+=str(result[i])
+		return int(e)
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	if(n<0):
+		n=abs(n)
+		a=(check(n,k,d))
+		return -abs(a)
+	else:
+		return check(n,k,d)
+
+
+
 
