e97d26d3459e1d5fadb028cc151c22733169091a
varuninaganti4598 <varuninaganti529@msitprogram.net>
2021-07-15 11:01:43 +0530

    Commit 1

--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -7,4 +7,11 @@
 
 def islegaltriangle(s1, s2, s3):
 	# your code goes here
-	pass
+	if(s1<0 or s2<0 or s3 < 0):
+    		return False
+	else:
+			if(s1+s2>s3 and s2+s3>s1 and s1+s3>s2):
+					return True
+			else:
+    				return False
+
