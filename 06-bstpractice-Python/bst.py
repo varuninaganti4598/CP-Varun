fbba72c03ece25ec5218e3475e3796aa913dc103
varuninaganti4598 <varuninaganti529@msitprogram.net>
2021-07-29 18:51:38 +0530

    commit 1

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,35 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        new_Node=Node(new_val)
+        if(self.root==None):
+            self.root=new_Node
+        else:
+            current=self.root
+            root1=self.root
+            while(current!=None):
+                root1=current
+                if(new_val<current.value):
+                    current = current.left
+                else:
+                    current=current.right
+            if(new_val<root1.value):
+                root1.left=new_Node
+            else:
+                root1.right=new_Node
 
     def printSelf(self):
         # Your code goes here
-        pass
+        print(self.root)
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        while(self.root!=None):
+            if(self.root.value==find_val):
+                return True
+            if(self.root.value<find_val):
+                self.root=self.root.right
+            else:
+                self.root=self.root.left
+        return False
 
