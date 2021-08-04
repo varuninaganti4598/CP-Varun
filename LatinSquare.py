a9a0fd9a6bcb61ec454fd7b1f59e64c95f985a7f
varuninaganti4598 <varuninaganti529@msitprogram.net>
2021-08-04 21:37:25 +0530

    commit 1

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,22 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    if(len(lst)==0):
+        return None
+    else:
+        row=len(lst)
+        col=len(lst[0])
+        c=0
+        for i in lst:
+            a=set(i)
+            if(len(a)==col):
+                c+=1
+            else:
+                return False
+        return True
+
+assert isLatinSquare([[1,2,3],[2,3,1],[3,1,2]])== True
+assert isLatinSquare([[1,2,1],[2,3,1],[3,1,2]])== False
+assert isLatinSquare([[],[2,3,1],[3,1,2]])== False
+assert isLatinSquare([])== None
+print("All test cases passed!")
